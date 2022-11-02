00f999694a4d7d0b5675ec315893710f666db185
PetyrB2 <peter.i.broadbent@gmail.com>
2022-11-02 11:23:11 +0000

    Added Exponents and examples

---
+++ src/Basics/Exponents.java
@@ -0,0 +1,43 @@
+package Basics;
+
+public class Exponents {
+
+	// An example of how to get a number to the Power of n
+	
+	public static void main(String[] args) {
+		System.out.println("========================");
+		System.out.println("Title: Exponent Examples");
+		System.out.println("Genre: Java Basics");
+		System.out.println("Author: Peter Broadbent");
+		System.out.println("Language: Java \n" + "Version: " + System.getProperty("java.version"));
+		System.out.println("========================\n");
+		System.out.println("5 to the power of 2: ");
+		System.out.println("\t= " + Math.pow(5, 2)); // (5 * 5) - Should be 25.0
+		System.out.println("");
+		System.out.println("3 to the power of 3: ");
+		System.out.println("\t= " + Math.pow(3, 3)); // (3 * 3 * 3) - Should be 27.0
+		System.out.println("");
+		System.out.println("As an int: ");
+		System.out.println("\t= " + (int)Math.pow(3, 3)); // (3 * 3 * 3) - Should be 27
+		System.out.println("");
+		System.out.println("5.5 to the power of 6.87 as a Double: ");
+		System.out.println("\t= " + (double)Math.pow(5.5, 6.87)); 
+		System.out.println("");
+		System.out.println("A different way of doing it. "
+				+ "\nCode = int exponent = (int)Math.pow(5, 5); " 
+				+ "\n5 to the power of 5: ");
+		int exponent = (int)Math.pow(5, 5);
+		System.out.println("\t= " + exponent);
+		System.out.println("");
+		System.out.println("A different way of doing it (as a Double)."
+				+ "\nCode = double exponent = (double)Math.pow(2.2, 5.1);"
+				+ "\n2.2 to the power of 5.1");
+		double exponentDouble = (double)Math.pow(2.2, 5.1);
+		System.out.println("\t= " + exponentDouble);
+		System.out.println("\n-------------------------------------------");
+		
+		
+	
+	}
+
+}
--- src/Basics/SomeMaths.java
+++ src/Basics/SomeMaths.java
@@ -3,25 +3,26 @@ package Basics;
 import java.util.Scanner;
 
 public class SomeMaths {
-	
-	
-	 // Method to find factorial of given number
-	 static int factorial(int n)
-	 {
-	  int res = 1, i;
-	  for (i=2; i<=n; i++)
-	   res *= i;
-	  return res;
-	 }
-	  
-	 // Driver method
-	 public static void main(String[] args)
-	 {
-		 Scanner scanner  = new Scanner(System.in);
-		 
-		 System.out.println("Please Enter a Number: ");
-		 int num = scanner.nextInt();
-	
-		System.out.println("The Factorial of --> "+ num + " <--  = " + factorial(num));
-	 }
+
+	// Method to find factorial of given number
+	static int factorial(int n) {
+		int res = 1, i;
+		for (i = 2; i <= n; i++)
+			res *= i;
+		return res;
 	}
+
+	// Driver method
+	public static void main(String[] args) {
+		
+//		int data [] = {1,2,4,5};
+		
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter a Number: ");
+		int num = scanner.nextInt();
+
+		System.out.println("\tThe Factorial of " + num + " is \n\t --> " + factorial(num) + " <-- ");
+	}
+}
